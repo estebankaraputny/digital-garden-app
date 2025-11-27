ca93e3b7862bfa555ca5374239778b5cbc9a67dc
estebankaraputny <estebankaraputny18@gmail.com>
2025-11-27 12:11:42 -0300

    :sparkles: Add: function notes in front

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 
 async function bootstrap() {
@@ -7,6 +8,25 @@ async function bootstrap() {
   
   app.enableCors();
   app.setGlobalPrefix('api/v1');
+
+
+
+  //  const options = new DocumentBuilder()
+  //   .setTitle('Digital Garden API')
+  //   .setDescription('The Digital Garden API description')
+  //   .setVersion('1.0')
+  //   .addBearerAuth({
+  //     type: 'http',
+  //     scheme: 'bearer',
+  //     bearerFormat: 'JWT',
+  //     in: 'header',
+  //     name: 'Authorization',
+  //     description: 'enter your JWT token',
+  //   },'bearer')
+  //   // .addSecurityRequirements('bearer')
+  //   .build();
+  // const document = SwaggerModule.createDocument(app, options);
+  // SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
--- backend/src/note/dto/create-note.dto.ts
+++ backend/src/note/dto/create-note.dto.ts
@@ -5,13 +5,13 @@ export class CreateNoteDto {
     @IsString({ message:'El titulo debe ser un texto'})
     @IsNotEmpty({message:'El titulo es obligatorio'})
     @MinLength(2, { message: 'El titulo debe tener más de 2 caracteres'})
-    @MaxLength(20, { message: 'El titulo debe tener menos de 20 caracteres'})
+    @MaxLength(50, { message: 'El titulo debe tener menos de 20 caracteres'})
     title: string;
 
     @IsString({ message:'El contenido debe ser un texto'})
     @IsNotEmpty({message:'El contenido es obligatorio'})
     @MinLength(20, { message: 'El contenido debe tener más de 20 caracteres'})
-    @MaxLength(400, { message: 'El contenido debe tener menos de 400 caracteres'})
+    @MaxLength(1000, { message: 'El contenido debe tener menos de 400 caracteres'})
     content: string;
 
     @IsString({ message:'La categoria debe ser un texto'})
@@ -23,7 +23,7 @@ export class CreateNoteDto {
     @IsString({ message:'La issue debe ser un texto'})
     @IsNotEmpty({message:'La issue es obligatoria'})
     @MinLength(2, { message: 'La issue debe tener más de 2 caracteres'})
-    @MaxLength(20, { message: 'La issue debe tener menos de 20 caracteres'})
+    @MaxLength(30, { message: 'La issue debe tener menos de 20 caracteres'})
     issue: string;
 
     @IsOptional()
