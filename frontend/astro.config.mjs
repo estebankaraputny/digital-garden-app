e3db6991d0f0bd7e3e46ff60bf7ded137cc54dc9
estebankaraputny <estebankaraputny18@gmail.com>
2025-12-23 22:08:47 -0300

    :sparkles: Add: function contact emailjs

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,9 +1,43 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    devToolbar: {
+
+  output: 'server',
+  devToolbar: {
     enabled: false
+  },
+
+  env: {
+    schema: {
+      // Nota: He cambiado los nombres para que coincidan con tu archivo .env
+      // y he cambiado el acceso a 'secret' y el contexto a 'server'.
+
+      EMAILJS_SERVICE_ID: envField.string({ 
+        context: 'server', 
+        access: 'secret', 
+        optional: false 
+      }),
+
+      EMAILJS_TEMPLATE_ID: envField.string({ 
+        context: 'server', 
+        access: 'secret', 
+        optional: false 
+      }),
+
+      EMAILJS_PUBLIC_KEY: envField.string({ 
+        context: 'server', 
+        access: 'secret', 
+        optional: false 
+      }),
+
+      // Agregamos la Private Key que faltaba
+      EMAILJS_PRIVATE_KEY: envField.string({ 
+        context: 'server', 
+        access: 'secret', 
+        optional: false 
+      }),
     }
+  }
 });
